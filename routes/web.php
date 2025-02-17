9db5070d80d643fcdc6026979e5f8448d975fbf6
mero san <lilhutao9@gmail.com>
2025-02-17 14:04:23 +0200

    third commit

--- routes/web.php
+++ routes/web.php
@@ -27,12 +27,30 @@ route::post('/about', function(){
 });
 
 Route::get(uri: 'tasks', action: function(){
-    return view(view: 'tasks');
+    $tasks = DB::table(table: 'tasks')->get();
+    return view(view: 'tasks', data: compact(var_name: 'tasks'));
 });
 
 Route::post(uri: 'create', action: function(){
     $task_name = $_POST['name'];
     DB::table(table: 'tasks')->insert(values: ['name' => $task_name]);
-    return view(view: 'tasks');
+    return redirect()->back();
+});
+
+Route::post(uri: 'delete/{id}', action: function($id){
+    DB::table(table: 'tasks')->where(column: 'id',operator: $id)->delete();
+    return redirect()->back();
+});
+
+Route::post(uri: 'edit/{id}', action: function($id){
+    $task=DB::table(table: 'tasks')->where(column: 'id',operator: $id)->first();
+    $tasks=DB::table(table: 'tasks')->get();
+    return view(view: 'tasks', data: ['task' => $task , 'tasks' => $tasks]);
+});
+
+Route::post(uri: 'update', action: function(){
+    $id = $_POST['id'];
+    DB::table(table: 'tasks')->where('id','=',$id)->update(['name'=> $_POST['name']]);
+    return redirect(to: 'tasks');
 });
 
